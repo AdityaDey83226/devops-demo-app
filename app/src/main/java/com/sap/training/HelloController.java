5d521d6beb514fdcbb89d40a402d41d10426bf6a
Kalwarala Abhignan <kalwarala.abhignan@sap.com>
2026-08-27 19:57:43 +0530

    feat: add Spring Boot app (Application, HelloController, pom.xml)

---
+++ app/src/main/java/com/sap/training/HelloController.java
@@ -0,0 +1,18 @@
+package com.sap.training;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from SAP DevOps Training!";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "ok";
+    }
+}
